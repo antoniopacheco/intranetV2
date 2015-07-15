9b54eb33403dcb5e84578848e20aa4b9362d1c34
antonio pacheco <antonio_pacheco@outlook.com>
2015-07-15 14:24:58 -0700

    Agregado logo del cec y skin ipn

--- app/inicio/mainctrl.js
+++ app/inicio/mainctrl.js
@@ -20,27 +20,39 @@ angular
   		var yearbefore = year--;
       $scope.series = [year, yearbefore];
       $scope.data=[];
-      loadPagos();
       loadPoa();
 
       function applyPagos(data){
         var actual=[];
         var anterior=[];
         var meses =[];
+        var ingresoActual = 0;
+        var ingresoPasado = 0;
         data.forEach(function(entry){
           if(entry.actual == 0)
             return false;
+          ingresoActual += entry.actual;
+          ingresoPasado += entry.anterior;
           actual.push(entry.actual);
           anterior.push(entry.anterior);
           meses.push(entry.mes);
         });
+        var ingresoPorcentajeActual = Math.round(((ingresoActual-ingresoPasado)*100/ingresoPasado)*100)/100;
+        if (ingresoPorcentajeActual > 0){
+          $scope.flechaIngreso = 'text-green';
+        }else if(ingresoPorcentajeActual == 0){
+          $scope.flechaIngreso = 'text-yellow';
+        }else{
+          $scope.flechaIngreso = 'text-red';
+          ingresoPorcentajeActual = -1*ingresoPorcentajeActual;
+        }
+        $scope.ingresoPorcentajeActual = ingresoPorcentajeActual;
         $scope.data.push(actual);
         $scope.data.push(anterior);
         $scope.labels = meses;
       }
 
       function applyPoa(data){
-        console.log(data);
         var year = Object.keys(data.poa)[0];
         $scope.current_cursos = data.poa[year]['total']['cursos']['ofertado'];
         $scope.poa_cursos = data.poa[year]['total']['cursos']['programado'];
@@ -52,17 +64,9 @@ angular
 
         $scope.current_ingresos = data.poa[year]['total']['ingresos']['ofertado'];
         $scope.poa_ingresos = data.poa[year]['total']['ingresos']['programado'];
-        $scope.porcenta_ingresos = data.poa[year]['total']['ingresos']['porcentaje'];                       
+        $scope.porcenta_ingresos = data.poa[year]['total']['ingresos']['porcentaje'];
+        applyPagos(data.ingresos);                      
       }
-
-      function loadPagos(){
-        pagosSrv.getall()
-        .then(
-          function(data){
-            applyPagos(data.resumen);
-          }
-        )
-      } 
 
       function loadPoa(){
         poaSrv.getResume()
